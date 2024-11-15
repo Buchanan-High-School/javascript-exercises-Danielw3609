15daea9ae92b208df4a4504f341e3f8185e22baa
Daniel3609 <601044@buchananschools.com>
2024-11-15 11:49:39 -0500

    ex02

--- ex02/script.js
+++ ex02/script.js
@@ -8,10 +8,17 @@ function task1() {
 
 // Write your task 2 code inside this function!
 function task2() {
-  
+  console.log('running task 2')
+  let element = document.querySelector('#task2');
+  console.log('yippee')
+   element.classList.toggle("inverted");
 }
 
 // Write your task 3 code inside this function!
 function task3() {
   console.log("Running task 3");
+  var ul = document.getElementById('list');
+  var li = document.createElement('li');
+  li.appendChild(document.createTextNode('HAM'));
+  ul.appendChild(li);
 }
