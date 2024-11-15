6503052970c8f0327833dd4d5a3d509dec0c41cd
Daniel3609 <601044@buchananschools.com>
2024-11-15 11:49:23 -0500

    EX02

--- ex02/script.js
+++ ex02/script.js
@@ -1,11 +1,14 @@
 // Write your Task 1 code inside this function!
 function task1() {
   console.log("Running task 1");
+  let target = document.querySelector('#text-content');
+  console.log(target);
+  target.innerText ="WHY IT NO WORK WORK";
 }
 
 // Write your task 2 code inside this function!
 function task2() {
-  console.log("Running task 2");
+  
 }
 
 // Write your task 3 code inside this function!
